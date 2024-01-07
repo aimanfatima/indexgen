bbc6044ed589eeaae58a3cc9125208d108f78e11
Aiman Fatima <aimanfatimadl@gmail.com>
2024-01-06 23:13:09 -0800

    Add selectBranch feature & add favicon

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
+import requests
 from flask import request, jsonify, Blueprint, render_template
 from .github_apis import fetch_repo_contents, generate_markdown
-from .utils.github_utils import convert_repo_url_to_api_endpoint
+from .utils.github_utils import convert_repo_url_to_api_endpoint, get_user_and_repo_from_url
 
 api = Blueprint('api', __name__)
 
@@ -8,17 +9,40 @@ api = Blueprint('api', __name__)
 def index():
     return render_template('index.html')
 
+@api.route('/fetch-branches', methods=['POST'])
+def fetch_branches():
+    data = request.json
+
+    # Extract user and repo from the request data
+    user, repo = get_user_and_repo_from_url(data.get('repo_url'))
+
+    # Construct the GitHub API URL for fetching branches
+    branches_url = f"https://api.github.com/repos/{user}/{repo}/branches"
+
+    # Make a request to the GitHub API to fetch the branches
+    response = requests.get(branches_url)
+
+    if response.status_code == 200:
+        branches = response.json()
+        # Extract just the names of the branches
+        branch_names = [branch['name'] for branch in branches]
+        return jsonify(branch_names)
+    else:
+        # Handle errors or no branches found
+        return jsonify({"error": "Failed to fetch branches"}), response.status_code
+
 @api.route('/generate-markdown', methods=['POST'])
 def generate_linked_markdown():
     data = request.json  # Get JSON data from request
     repo_url = data.get("repo_url")
+    branch = data.get("branch")
 
     if not repo_url:
         return jsonify({"error": "No repo URL provided"}), 400
 
     # Assuming repo_url is directly usable or transform it as needed
     try:
-        contents_api_url = convert_repo_url_to_api_endpoint(repo_url)
+        contents_api_url = convert_repo_url_to_api_endpoint(repo_url, branch)
     except Exception as e:
         return jsonify({"error": str(e)}), 400
     
--- app/utils/github_utils.py
+++ app/utils/github_utils.py
@@ -19,5 +19,19 @@ def convert_repo_url_to_api_endpoint(repo_url, branch='master'):
 
     # Construct the API URL
     api_url = f"https://api.github.com/repos/{username}/{repository}/contents?ref={branch}"
-    
+    print(api_url)
     return api_url
+
+def get_user_and_repo_from_url(repo_url):
+    # Remove the trailing slash if it exists
+    if repo_url.endswith('/'):
+        repo_url = repo_url[:-1]
+
+    # Split the URL to get individual components
+    parts = repo_url.split('/')
+
+    # Extract the username and repository name
+    username = parts[-2]
+    repository = parts[-1]
+
+    return username, repository
